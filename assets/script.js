68137ecc6055349237f93eae62a26932b63b279e
Musa Muhammed Awwal <talk2muhammedawwal@gmail.com>
2024-12-31 17:18:52 +0100

    update script file

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,37 @@
-const selection = document.querySelectorAll('.selections');
+const selections = document.querySelectorAll('.selections');
 const currentPlayer = 'user';
-selection.forEach((selected)=>{
+const selectionPicks = ['paper','scissors','rock'];
+selections.forEach((selected)=>{
     selected.addEventListener('click',()=>{
-        console.log(selected.dataset.selectedvalue);
+        const pick = selected.dataset.selectedvalue;
+        if(selectionPicks.includes(pick)){
+            console.log(selected.dataset.selectedvalue);
+        }
+        document.querySelector('.gameContainer').classList.add('hide');
+        document.querySelector('.playing').classList.remove('hide');
     })
 })
 console.log('hi')
+document.querySelector('.ruleClose').addEventListener('click',()=>{
+    document.querySelector('.overlay').classList.add('hide')
+})
+function renderPlay(){
+
+}
+function checkWinner(userPick, housePick) {
+    if (userPick === housePick) return 'tie';
+
+    if (
+        (userPick === 'paper' && housePick === 'rock') ||
+        (userPick === 'rock' && housePick === 'scissors') ||
+        (userPick === 'scissors' && housePick === 'paper')
+    ) {
+        return 'user';
+    }
+
+    return 'house';
+}
+function houseMover(){
+   return Math.floor(Math.random() * 3);
+
+}
